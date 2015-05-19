00133e9131e8ac142079651e96aab2f551e36fe8
Dirk Schumacher <mail@dirk-schumacher.net>
2015-05-19 22:17:36 +0000

    Fix tests

--- tests/testthat/test-contains.R
+++ tests/testthat/test-contains.R
@@ -3,11 +3,11 @@ context("step functions")
 test_that("contains stops when element is found.", {
   result <- transduce(contains(function(x) x == 10), plus, 
                       create_sequence(0, function(x) x + 1), init = 0)
-  expect_that(result, 10)
+  expect_that(result, is_equivalent_to(10))
 })
 
 test_that("contains continues the process on the found element", {
   pipe <- compose(contains(function(x) x == 10), map(function(x) x + 20))
   result <- transduce(pipe, plus, 1:20, init = 0)
-  expect_that(result, 30)
+  expect_that(result, is_equivalent_to(30))
 })
