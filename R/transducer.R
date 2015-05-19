bd00e10f5f3ed40f0cae271198c5fe5c7ece9355
Dirk Schumacher <mail@dirk-schumacher.net>
2015-05-19 21:33:54 +0000

    Fix bug in example

--- R/transducer.R
+++ R/transducer.R
@@ -3,7 +3,7 @@
 #' Implements transducers introduced in Clojure as well as infinite sequences.
 #' 
 #' @examples
-#' transduce(take(5), plus_t, create_sequence(0, function(x) x + 1), init = 0)
+#' transduce(take(5), plus, create_sequence(0, function(x) x + 1), init = 0)
 #' @docType package
 #' @name transducer
 NULL
