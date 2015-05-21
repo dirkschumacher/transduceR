42a557e04362c28820881cedbc7504c926c3d6ea
Dirk Schumacher <mail@dirk-schumacher.net>
2015-05-21 21:49:38 +0000

    Add test for take_while

--- tests/testthat/test-take.R
+++ tests/testthat/test-take.R
@@ -8,3 +8,8 @@ test_that("take-nth takes every nth element.", {
 test_that("take-nth throws error if n < 1.", {
   expect_error(take_nth(0))
 })
+
+test_that("take-while works with basic example.", {
+  result <- transduce(take_while(function(x) x < 5), sum, 1:20)
+  expect_that(result, is_equivalent_to(sum(1:4)))
+})
